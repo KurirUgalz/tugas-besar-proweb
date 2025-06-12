6f02ce4d3c172e6603806c81745935179f3b1aa8
bayu <bayuunugroho14@gmail.com>
2025-06-12 20:44:02 +0700

    merubah daftar.php sesi 2

--- 3 - halamanlogin/daftar.php
+++ 3 - halamanlogin/daftar.php
@@ -38,3 +38,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+ad>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Daftar</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div class="wrapper">
+    <h1>Daftar</h1>
+    <p id="error-message" style="color: red;"><?php echo $error; ?></p>
+    <form id="form" method="POST" action="">
+      <div>
+        <label for="nama-input">
+          <!-- ikon -->
+        </label>
+        <input type="text" name="nama" id="nama-input" placeholder="Nama Lengkap" required />
+      </div>
+      <div>
+        <label for="email-input">
+          <span>@</span>
+        </label>
+        <input type="email" name="email" id="email-input" placeholder="Email" required />
+      </div>
+      <div>
+        <label for="password-input">
+          <!-- ikon -->
+        </label>
+        <input type="password" name="password" id="password-input" placeholder="Sandi" required />
+      </div>
+      <div>
+        <label for="repeat-password-input">
+          <!-- ikon -->
+        </label>
+        <input
+          type="password"
+          name="repeat-password"
+          id="repeat-password-input"
+          placeholder="Ulangi Sandi"
+          required
+        />
+      </div>
+      <button type="submit">Daftar</button>
+    </form>
+    <p>
+      Sudah punya akun? <a href="login.php">Masuk</a>
+    </p>
+  </div>
+</body>
+</html>
